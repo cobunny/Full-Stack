81e73d481a5a22fe9a390995298332ba3b8a5761
Cobunny <malatanggm@gmail.com>
2016-03-08 12:13:54 -0800

    Restaurant init

---
+++ vagrant/sqlalchemy_test/database_setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+		
+
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+		
+
+### columnNmae = Column(attributes, ...)
+### example attributes:
+### String(250), Integer, relationship(Class), nullable=False, primary_key=True, ForeignKey('some_table.id')
+
+	name = Column(String(80), nullable =False)
+	id = Column(Integer, primary_key=True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
+	restaurant = relationship(Restaurant)
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+	
+
+
+
+#### insert at end of file ####
+
+engine = create_engine()
+Base.metadata.create_all(engine)
