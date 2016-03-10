a70a0735f85479214eaa809f1a71e71de2a48a0b
Cobunny <malatanggm@gmail.com>
2016-03-10 11:03:47 -0800

    Jsonify

--- vagrant/sqlalchemy_test/database_setup.py
+++ vagrant/sqlalchemy_test/database_setup.py
@@ -8,31 +8,41 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Restaurant(Base):
-	__tablename__ = 'restaurant'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-		
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
-		
+    __tablename__ = 'menu_item'
 
-### columnNmae = Column(attributes, ...)
-### example attributes:
-### String(250), Integer, relationship(Class), nullable=False, primary_key=True, ForeignKey('some_table.id')
+    # columnNmae = Column(attributes, ...)
+    # example attributes:
+    # String(250), Integer, relationship(Class), nullable=False,
+    # primary_key=True, ForeignKey('some_table.id')
 
-	name = Column(String(80), nullable =False)
-	id = Column(Integer, primary_key=True)
-	course = Column(String(250))
-	description = Column(String(250))
-	price = Column(String(8))
-	restaurant = relationship(Restaurant)
-	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-	
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant = relationship(Restaurant)
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 
+    @property
+    def serialize(self):
+        """Returns object data in easily serializeable format
+		"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 
 #### insert at end of file ####
