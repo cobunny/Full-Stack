6cff14bd8ecaf0d4416addc609107d73221e8518
Cobunny <malatanggm@gmail.com>
2016-03-22 13:30:55 -0700

    Jsonify

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -7,11 +7,20 @@ from sqlalchemy.dialects.drizzle import TIMESTAMP
 
 Base = declarative_base()
 
+
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format for Jsonify"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
 
 
 class CategoryItem(Base):
@@ -23,6 +32,17 @@ class CategoryItem(Base):
     lastTime = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format for Jsonify"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+
+        }
+
 
 engine = create_engine('sqlite:///catalog.db')
 
