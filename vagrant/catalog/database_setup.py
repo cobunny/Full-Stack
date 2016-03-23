eabae23b77d6a93c3568a37c85fedc036f19dcd9
Cobunny <malatanggm@gmail.com>
2016-03-22 23:11:32 -0700

    Added user, log-in page and public html pages

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -8,11 +8,22 @@ from sqlalchemy.dialects.drizzle import TIMESTAMP
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -20,6 +31,7 @@ class Category(Base):
         return {
             'id': self.id,
             'name': self.name,
+            'user_id': self.user_id
         }
 
 
@@ -32,6 +44,8 @@ class CategoryItem(Base):
     lastTime = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -44,6 +58,6 @@ class CategoryItem(Base):
         }
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('sqlite:///catalogwithusers.db')
 
 Base.metadata.create_all(engine)
